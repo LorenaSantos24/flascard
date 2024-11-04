509d92806a577ea502b9cb61c23dcf519467e94b
Lorena <00001095650014sp@al.educacao.sp.gov.br>
2024-11-04 10:41:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'literatura',
+    'Quem escreveu Dom Casmurro?',
+    'machado de assis'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual o simbola quimico da agua?',
+    'h2o'
+)
+
+
+criaCartao(
+    'Brasil',
+    'Qual e a capital do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'eu te amo em ingles é I LOVE YOU'
+)
